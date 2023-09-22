8caf0a892bc05c857541a75008cc1b94cf45b3fc
Fajra Risqulla <fajrarisqulla@gmail.com>
2023-09-22 22:38:39 +0700

    fix overflow in favorite page

--- lib/presentation/pages/favorite/favorite_page.dart
+++ lib/presentation/pages/favorite/favorite_page.dart
@@ -28,13 +28,15 @@ final class FavoritePage extends ConsumerWidget {
                     ),
               ),
             ),
-            child: _buildAdaptiveBody(
-              context,
-              favoriteNoteState,
-              navBarNotifier,
-              onDismissed: (_, favoriteNote) => ref.read(
-                swipeNoteProvider(
-                  favoriteNote: favoriteNote.copyWith(isFavorite: false),
+            child: SafeArea(
+              child: _buildAdaptiveBody(
+                context,
+                favoriteNoteState,
+                navBarNotifier,
+                onDismissed: (_, favoriteNote) => ref.read(
+                  swipeNoteProvider(
+                    favoriteNote: favoriteNote.copyWith(isFavorite: false),
+                  ),
                 ),
               ),
             ),
@@ -49,34 +51,37 @@ final class FavoritePage extends ConsumerWidget {
               ),
               centerTitle: true,
             ),
-            body: _buildAdaptiveBody(
-              context,
-              favoriteNoteState,
-              navBarNotifier,
-              onDismissed: (_, favoriteNote) {
-                ref.read(
-                  swipeNoteProvider(
-                    favoriteNote: favoriteNote.copyWith(isFavorite: false),
-                  ),
-                );
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    backgroundColor:
-                        Theme.of(context).colorScheme.inverseSurface,
-                    action: SnackBarAction(
-                      label: 'Undo',
-                      onPressed: () => ref.read(
-                        swipeNoteProvider(
-                          favoriteNote: favoriteNote.copyWith(isFavorite: true),
+            body: SafeArea(
+              child: _buildAdaptiveBody(
+                context,
+                favoriteNoteState,
+                navBarNotifier,
+                onDismissed: (_, favoriteNote) {
+                  ref.read(
+                    swipeNoteProvider(
+                      favoriteNote: favoriteNote.copyWith(isFavorite: false),
+                    ),
+                  );
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      backgroundColor:
+                          Theme.of(context).colorScheme.inverseSurface,
+                      action: SnackBarAction(
+                        label: 'Undo',
+                        onPressed: () => ref.read(
+                          swipeNoteProvider(
+                            favoriteNote:
+                                favoriteNote.copyWith(isFavorite: true),
+                          ),
                         ),
                       ),
+                      content: Text(
+                        'Successfully removed ${favoriteNote.title} from the favorites',
+                      ),
                     ),
-                    content: Text(
-                      'Successfully removed ${favoriteNote.title} from the favorites',
-                    ),
-                  ),
-                );
-              },
+                  );
+                },
+              ),
             ),
           );
   }
@@ -89,9 +94,10 @@ final class FavoritePage extends ConsumerWidget {
         onDismissed,
   }) =>
       favoriteNoteState.whenOrNull(
-            data: (favoriteNotes) => favoriteNotes.isEmpty
-                ? Center(
-                  child: Column(
+        data: (favoriteNotes) => favoriteNotes.isEmpty
+            ? ListView(
+                children: [
+                  Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       const Image(
@@ -105,49 +111,46 @@ final class FavoritePage extends ConsumerWidget {
                         child: Text(
                           '❤️ Add your favorite notes.\nHappy writing! 🌟',
                           textAlign: TextAlign.center,
-                          style: Theme.of(context)
-                              .textTheme
-                              .labelLarge
-                              ?.copyWith(
-                            color: Theme.of(context)
-                                .adaptiveOnSurface(context),
-                          ),
+                          style:
+                              Theme.of(context).textTheme.labelLarge?.copyWith(
+                                    color: Theme.of(context)
+                                        .adaptiveOnSurface(context),
+                                  ),
                         ),
                       )
                     ],
                   ),
-                )
-                : SafeArea(
-                  child: Padding(
-                      padding: const EdgeInsets.only(
-                        left: 16.0,
-                        right: 16.0,
-                        top: 16.0,
-                        bottom: 0.0,
-                      ),
-                      child: ListView.separated(
-                        // swipe to remove from the favorites
-                        itemBuilder: (context, index) => FavoriteCard(
-                          title: favoriteNotes[index].title,
-                          body: favoriteNotes[index].body,
-                          dismissibleKey: ObjectKey(favoriteNotes[index]),
-                          favoriteNote: favoriteNotes[index],
-                          onTap: () {
-                            navBarNotifier.state = false;
-                            context.go(
-                              '${NavDestination.favorite.path}/${favoriteNotes[index].id}',
-                            );
-                          },
-                          onDismissed: (direction) =>
-                              onDismissed(direction, favoriteNotes[index]),
-                        ),
-                        separatorBuilder: (context, index) => const Divider(),
-                        itemCount: favoriteNotes.length,
-                      ),
-                    ),
+                ],
+              )
+            : Padding(
+                padding: const EdgeInsets.only(
+                  left: 16.0,
+                  right: 16.0,
+                  top: 16.0,
+                  bottom: 0.0,
                 ),
-          ) ??
-          Center(
-            child: Theme.of(context).adaptiveCircularProgress(context),
-          );
+                child: ListView.separated(
+                  // swipe to remove from the favorites
+                  itemBuilder: (context, index) => FavoriteCard(
+                    title: favoriteNotes[index].title,
+                    body: favoriteNotes[index].body,
+                    dismissibleKey: ObjectKey(favoriteNotes[index]),
+                    favoriteNote: favoriteNotes[index],
+                    onTap: () {
+                      navBarNotifier.state = false;
+                      context.go(
+                        '${NavDestination.favorite.path}/${favoriteNotes[index].id}',
+                      );
+                    },
+                    onDismissed: (direction) =>
+                        onDismissed(direction, favoriteNotes[index]),
+                  ),
+                  separatorBuilder: (context, index) => const Divider(),
+                  itemCount: favoriteNotes.length,
+                ),
+              ),
+      ) ??
+      Center(
+        child: Theme.of(context).adaptiveCircularProgress(context),
+      );
 }
